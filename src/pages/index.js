f3e95d120b504e5ab85e660b0dbb6517be5d9d1d
kahil nayton <kahilnayton@gmail.com>
2020-01-09 23:30:35 -0500

    started the gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ import Svg from "../Icons/AbacusWithStand"
 import Svg2 from "../Icons/BigInvoice"
 import Svg3 from "../Icons/BigTelephone"
 import Svg4 from "../Icons/BigCloud"
-import Svg5 from "../Icons/Funds"
-import Svg6 from "../Icons/CurriculumWithPicture"
+import Svg5 from "../Icons/InclinedKey"
+import Svg6 from "../Icons/InclinedClip"
 import Svg7 from "../Icons/Funds"
 import Svg8 from "../Icons/ListWithText"
 import Svg9 from "../Icons/WebHome"
@@ -25,6 +25,16 @@ import image6 from "../images/image6.jpg"
 import image7 from "../images/image7.jpeg"
 import image8 from "../images/image8.jpeg"
 import image9 from "../images/image9.jpeg"
+import image10 from "../images/image10.jpeg"
+
+import house1 from "../images/house1.jpeg"
+import house2 from "../images/house2.jpeg"
+import house3 from "../images/house3.jpeg"
+import house4 from "../images/house4.jpeg"
+import house5 from "../images/house5.jpeg"
+import house6 from "../images/house6.jpeg"
+import house7 from "../images/house7.jpeg"
+// import house8 from "../images/house8.jpeg"
 
 const IndexPage = () => (
   <>
@@ -32,7 +42,6 @@ const IndexPage = () => (
       <div className="sidebar">sidebar</div>
       <div className="header">Header</div>
       <div className="realtors">Top 3!</div>
-
       <section className="features">
         <div className="feature">
           <Svg className="feature__icon" />
@@ -103,8 +112,7 @@ const IndexPage = () => (
         </p>
         <button className="btn">Find your own home</button>
       </div>
-
-      // home 1
+      {/* // home 1 */}
       <section className="homes">
         <div className="home">
           <img src={image5} alt="home 1" className="home__img" />
@@ -115,24 +123,23 @@ const IndexPage = () => (
             <Svg6 className="home__location--svg svg" />
             <p>USA</p>
           </div>
+          <div className="home__rooms">
+            <Svg7 className="home__rooms--svg svg" />
+            <p>5 rooms</p>
+          </div>
+          <div className="home__area">
+            <Svg8 className="home__rooms--svg svg" />
+            <p>
+              459 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <Svg9 className="home__rooms--svg svg" />
+            <p>50$</p>
+          </div>
+          <button className="btn home__btn">Contact realtor</button>
         </div>
-        <div className="home__rooms">
-          <Svg7 className="home__rooms--svg svg" />
-          <p>5 rooms</p>
-        </div>
-        <div className="home__area">
-          <Svg8 className="home__rooms--svg svg" />
-        </div>
-        <p>
-          459 m<sup>2</sup>
-        </p>
-        <div className="home__price">
-          <Svg9 className="home__rooms--svg svg" />
-          <p>50$</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
-
-        // home 2
+        {/* // home 2 */}
         <div className="home">
           <img src={image6} alt="home 1" className="home__img" />
           <Svg5 className="home__like" />
@@ -141,24 +148,23 @@ const IndexPage = () => (
             <Svg6 className="home__like" />
             <p>Canada</p>
           </div>
+          <div className="home__rooms">
+            <Svg7 className="home__rooms--svg svg" />
+            <p>3 rooms</p>
+          </div>
+          <div className="home__area">
+            <Svg8 className="home__rooms--svg svg" />
+            <p>
+              559 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <Svg9 className="home__rooms--svg svg" />
+            <p>50, 000$</p>
+          </div>
+          <button className="btn home__btn">Contact realtor</button>
         </div>
-        <div className="home__rooms">
-          <Svg7 className="home__rooms--svg svg" />
-          <p>3 rooms</p>
-        </div>
-        <div className="home__area">
-          <Svg8 className="home__rooms--svg svg" />
-        </div>
-        <p>
-          559 m<sup>2</sup>
-        </p>
-        <div className="home__price">
-          <Svg9 className="home__rooms--svg svg" />
-          <p>50, 000$</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
-
-        // home 3
+        {/* // home 3 */}
         <div className="home">
           <img src={image7} alt="home 1" className="home__img" />
           <Svg5 className="home__like" />
@@ -167,24 +173,23 @@ const IndexPage = () => (
             <Svg6 className="home__like" />
             <p>Spain</p>
           </div>
+          <div className="home__rooms">
+            <Svg7 className="home__rooms--svg svg" />
+            <p>7 rooms</p>
+          </div>
+          <div className="home__area">
+            <Svg8 className="home__rooms--svg svg" />
+            <p>
+              1159 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <Svg9 className="home__rooms--svg svg" />
+            <p>1,500,000$</p>
+          </div>
+          <button className="btn home__btn">Contact realtor</button>
         </div>
-        <div className="home__rooms">
-          <Svg7 className="home__rooms--svg svg" />
-          <p>7 rooms</p>
-        </div>
-        <div className="home__area">
-          <Svg8 className="home__rooms--svg svg" />
-        </div>
-        <p>
-          1159 m<sup>2</sup>
-        </p>
-        <div className="home__price">
-          <Svg9 className="home__rooms--svg svg" />
-          <p>1,500,000$</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
-
-        // home 4
+        {/* // home 4 */}
         <div className="home">
           <img src={image8} alt="home 1" className="home__img" />
           <Svg5 className="home__like" />
@@ -193,23 +198,23 @@ const IndexPage = () => (
             <Svg6 className="home__like" />
             <p>USA</p>
           </div>
+          <div className="home__rooms">
+            <Svg7 className="home__rooms--svg svg" />
+            <p>2 rooms</p>
+          </div>
+          <div className="home__area">
+            <Svg8 className="home__rooms--svg svg" />
+            <p>
+              159 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <Svg9 className="home__rooms--svg svg" />
+            <p>50,000$</p>
+          </div>
+          <button className="btn home__btn">Contact realtor</button>
         </div>
-        <div className="home__rooms">
-          <Svg7 className="home__rooms--svg svg" />
-          <p>2 rooms</p>
-        </div>
-        <div className="home__area">
-          <Svg8 className="home__rooms--svg svg" />
-        </div>
-        <p>
-          159 m<sup>2</sup>
-        </p>
-        <div className="home__price">
-          <Svg9 className="home__rooms--svg svg" />
-          <p>50,000$</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
-        // home 5
+        {/* // home 5 */}
         <div className="home">
           <img src={image9} alt="home 1" className="home__img" />
           <Svg5 className="home__like" />
@@ -218,24 +223,57 @@ const IndexPage = () => (
             <Svg6 className="home__like" />
             <p>Australia</p>
           </div>
+          <div className="home__rooms">
+            <Svg7 className="home__rooms--svg svg" />
+            <p>3 rooms</p>
+          </div>
+          <div className="home__area">
+            <Svg8 className="home__rooms--svg svg" />
+            <p>
+              759 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <Svg9 className="home__rooms--svg svg" />
+            <p>$600,000</p>
+          </div>
+          <button className="btn home__btn">Contact realtor</button>
         </div>
-        <div className="home__rooms">
-          <Svg7 className="home__rooms--svg svg" />
-          <p>3 rooms</p>
+
+        {/* // home 5 */}
+        <div className="home">
+          <img src={image10} alt="home 1" className="home__img" />
+          <Svg5 className="home__like" />
+          <h5 className="home__name">A frame house</h5>
+          <div className="home__location">
+            <Svg6 className="home__like" />
+            <p>Australia</p>
+          </div>
+          <div className="home__rooms">
+            <Svg7 className="home__rooms--svg svg" />
+            <p>3 rooms</p>
+          </div>
+          <div className="home__area">
+            <Svg8 className="home__rooms--svg svg" />
+            <p>
+              759 m<sup>2</sup>
+            </p>
+          </div>
+          <div className="home__price">
+            <Svg9 className="home__rooms--svg svg" />
+            <p>$600,000</p>
+          </div>
+          <button className="btn home__btn">Contact realtor</button>
         </div>
-        <div className="home__area">
-          <Svg8 className="home__rooms--svg svg" />
-        </div>
-        <p>
-          759 m<sup>2</sup>
-        </p>
-        <div className="home__price">
-          <Svg9 className="home__rooms--svg svg" />
-          <p>$600,000</p>
-        </div>
-        <button className="btn home__btn">Contact realtor</button>
       </section>
-      <section className="gallery">Gallery</section>
+      <section className="gallery">
+
+        <figure className="gallery__item gallery__item--1">
+        <img src={house1} alt="Gallery image 1" className="gallery__img" />
+        </figure>
+
+        <img src={house2} alt="Gallery image 2" className="gallery__img" />
+      </section>
       <footer className="footer">Footer</footer>
       <div className="medication-ticker"></div>
     </div>
